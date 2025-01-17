460685468df25091a30c5968a36989240c3de9fb
Obnostic <unklefoddy@gmail.com>
2025-01-16 22:39:45 -0500

    partial

--- js/assignment.js
+++ js/assignment.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const submissionBtn = document.getElementById("submission-btn");
-const remainderElement = document.getElementById("remainder");
+/* const remainderElement = document.getElementById("remainder");
 const checkIfEvenElement = document.getElementById("check-if-even");
-const lostNumbersElement = document.getElementById("lost-numbers");
+const lostNumbersElement = document.getElementById("lost-numbers");         UNUSED */
 
 
 function returnTheRemainder(num1, num2) {
@@ -11,19 +11,14 @@ function returnTheRemainder(num1, num2) {
 }
 
 
-function checkIfEven(num3) {
-
-    
+function checkIfEven(num3) {    
     return (num3 % 2 === 0);
-
 }
 
 
 function getTheFourthElement(num4) {
-
     const lostNumbers = [4, 8, 15, 16, 23, 42];
     return(lostNumbers[num4]);
-
 }
 
 
