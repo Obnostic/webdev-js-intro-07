97bd8307c6370928637bec35f0b0657a59155e62
Steven <unklfoddy@gmail.com>
2025-01-17 12:17:41 -0500

    Completed.

--- js/index.js
+++ js/index.js
@@ -2,30 +2,29 @@
 // that are not declared
 "use strict"
 
-/*
+//Create some functions return a value and log that value to the console.
 function add(num1, num2) {
     return num1 + num2;
 }
 
-
+//Create some functions that take in arguments, but also have default parameters.
 function greet(name = 'Guest') {
     console.log('Hello, ' + name + '!');
 }
 
-let add2 = (num1) => num1+6;
-*/
-
-
-// display start 
-
-
-//Create some functions return a value and log that value to the console.
-//Create some functions that take in arguments. Try to write at one least 
+//Create some functions that take in arguments. Try to write at least 
 // one that returns a value and one that doesn't.
-//Create some functions that take in arguments, but also have default parameters.
+function greet2(name) {
+    console.log('Hello, ' + name + '!');
+}
 
 
-/*
+
+
+
+
+
+
 console.log("SUM:");
 let sum = add(2, 4);
 console.log(sum);        // Output: 6 
@@ -35,7 +34,8 @@ console.log("GREET:");
 greet();         // Output: "Hello, Guest!"
 greet("Dan");    // Output: "Hello, Dan!"
 
+console.log("GREET2 with no default:");
+greet2();         // Output: "Hello, undefined!"
+greet2("Dan");    // Output: "Hello, Dan!"
 
-console.log("ARROW, ADD2:")
-console.log(add2(3));
-*/
+
